df2058c9b588fa1217fea472363d51567cd6b364
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-11-03 16:13:57 -0300

    Added prototype

---
+++ src/prototype/Client.java
@@ -0,0 +1,25 @@
+package prototype;
+
+
+public class Client {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+        Document d1 = new Document();
+        d1.name = "Name 1";
+        d1.visibility = "PRIVATE";
+        d1.type = "PDF";
+        d1.path = "C:/";
+        
+        System.out.println(d1);
+        
+        System.out.println("--------------");
+        
+        Document d2 = d1.clone();
+        d2.name = "Name 2";
+        
+        System.out.println(d2);
+        System.out.println(d1);
+        
+	}
+
+}
---
+++ src/prototype/Document.java
@@ -0,0 +1,19 @@
+package prototype;
+
+public class Document implements Cloneable {
+	String name;
+	String visibility;
+	String type;
+	String path;
+	
+	@Override
+	protected Document clone() throws CloneNotSupportedException {
+		return (Document) super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return "Document [nome=" + name + ", visibilidade=" + visibility + ", tipo=" + type + ", localizacao="
+				+ path + "]";
+	}
+}
