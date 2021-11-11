37f77d6b489b9946bf818970e60f243fa7733fa0
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-11-11 15:00:18 -0300

    Null object design pattern

---
+++ src/behavioral/nullobject/Client.java
@@ -0,0 +1,11 @@
+package behavioral.nullobject;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ComplexService service = new ComplexService("Simple report 2",new NullStorageService());
+		service.generateReport();
+		
+	}
+
+}
